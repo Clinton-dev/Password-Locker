f04c61fbb09d5cb5eb236db7e48caff76588ce7f
Clinton wambugu <clintonwambugu@gmail.com>
2022-04-23 21:36:14 +0300

    Add init test on credential class

--- credentials_test.py
+++ credentials_test.py
@@ -12,10 +12,23 @@ class TestCredential(unittest.TestCase):
         """
         Set up method to run before each test case
         """
-        self.new_user = Credential("Clinton","comPlex!")
+        self.new_credential = Credential("twitter","pass123","john.doe@ymail")
 
     def tearDown(self):
         """
         tearDown method cleans up after each test case
         """
         Credential.credential_list = []
+
+    def test_init(self):
+        """
+        test_init test case to test if object is intialized properly
+        """
+
+        self.assertEqual(self.new_credential.name,"twitter")
+        self.assertEqual(self.new_credential.password, "pass123")
+        self.assertEqual(self.new_credential.email, "john.doe@ymail")
+
+
+if __name__ == "__main__":
+    unittest.main()
