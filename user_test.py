3bad16c812ae5fc40291335b2eb85f4a71898cf7
Clinton wambugu <clintonwambugu@gmail.com>
2022-04-23 20:30:04 +0300

    Add init test for user class

--- user_test.py
+++ user_test.py
@@ -8,3 +8,20 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     """
+
+    def setUp(self):
+        """
+        Set up method to run before each test case
+        """
+        self.new_user = User("Clinton","comPlex!")
+
+    def test_init(self):
+        """
+        test_init test case to test if object is intialized properly
+        """
+
+        self.assertEqual(self.new_user.username,"Clinton")
+        self.assertEqual(self.new_user.password, "comPlex!")
+
+if __name__ == "__main__":
+    unittest.main()
