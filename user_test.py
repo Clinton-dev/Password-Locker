d07e1c1a53b12d74ed10b853583fc035c14cd3f9
Clinton wambugu <clintonwambugu@gmail.com>
2022-04-23 20:16:10 +0300

    Add user test class

---
+++ user_test.py
@@ -0,0 +1,10 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test case for User class behaviors
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
